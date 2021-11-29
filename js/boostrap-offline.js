93d374c61107da38e1887ef3f95465d5d992f4f1
yeniferAlvarado <yeniferday99@gmail.com>
2021-11-29 12:12:10 -0500

    Acomode Bootstrap

---
+++ js/boostrap-offline.js
@@ -0,0 +1,58 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+   
+    window.addEventListener('load', function(){
+      /*   Alert */
+      var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+      var alertTrigger = document.getElementById('liveAlertBtn')
+      
+      function alert(message, type) {
+        var wrapper = document.createElement('div')
+        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+      
+        alertPlaceholder.append(wrapper)
+      }
+      
+      if (alertTrigger) {
+        alertTrigger.addEventListener('click', function () {
+          alert('Alerta generada por Boostrap!', 'success')
+        })
+      }
+
+      /* validacion */
+    var forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  });
+window.addEventListener("load", function(){ 
+    /*  Plugin Tooltip */
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    /* Pluion de Popers */
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+
+    /*  Pluii Toast */
+    var toastTrigger = document.getElementById('liveToastBtn')
+      var toastLiveExample = document.getElementById('liveToast')
+      if (toastTrigger) {
+        toastTrigger.addEventListener('click', function () {
+          var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+});
